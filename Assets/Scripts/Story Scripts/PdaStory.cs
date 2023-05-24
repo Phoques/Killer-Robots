feeef388cd12a484176b1b66d61595d7f613ed79
Phoques <109713674+Phoques@users.noreply.github.com>
2023-05-24 17:49:35 +1000

    Huge code update
    PDA system working

--- Assets/Scripts/Story Scripts/PdaStory.cs
+++ Assets/Scripts/Story Scripts/PdaStory.cs
@@ -1,33 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PdaStory : MonoBehaviour
 {
 
     HudText hudText;
+    RaycastHit hit;
+    public LayerMask interactable;
+
+    public GameObject PDAPanel;
+    public Button PDA1LogButton;
+    public Button PDA2LogButton;
+    public Text PDAPanelText;
+    PlayerMovement playerMovementClass;
 
 
-    public bool isPDA1;
-    bool isPDA2;
-    bool isPDA3;
+
+
+    //May need an array to close em but then later on notebook might not work
+    //public bool PDAFound = false;
+    public bool isPDA1 = false;
+    public bool isPDA2 = false;
+    public bool isPDA3 = false;
+
+    public bool PDA1Found = false;
+    public bool PDA2Found = false;
 
     private void Start()
     {
         hudText = FindObjectOfType<HudText>();
+        PDAPanel.gameObject.SetActive(false);
+        playerMovementClass = FindObjectOfType<PlayerMovement>();
+        PDAButtonLogHide();
+
     }
 
-    public void PDACheck()
+
+    private void Update()
     {
-        if (isPDA1)
+        if (Input.GetKeyDown(KeyCode.L))
         {
-            PDA1();
+
+            DisplayPDALog();
+            CheckPDALog();
+            playerMovementClass.UnlockCursor();
         }
+    }
+
+    private void DisplayPDALog()
+    {
+        PDAPanel.gameObject.SetActive(true);
+        playerMovementClass.canMove = false;
+
+    }
+
+    public void ClosePDALog()
+    {
+        playerMovementClass.LockCursor();
+        PDAPanel.gameObject.SetActive(false);
+        playerMovementClass.canMove = true;
     }
 
     public void PDA1()
     {
         hudText.noteText.text = "Captains Log, 19081271 \n I found a donut the other day, boy was it tasty, omg killer robots someone HALP pls send the fkn Space marines or some shit \n Oh well im ded now fk";
         hudText.hudExitText.text = "Press (X) to close PDA notes";
+
     }
+
+    public void PDA2()
+    {
+        hudText.noteText.text = "Scientist log 1234 \n Robuts, they are gaining teh smarts \n Need to fix";
+        hudText.hudExitText.text = "Press (X) to close PDA notes";
+
+    }
+
+    //Maybe look into finding a way to make it in the order found (non goal)
+    public void PDA1Log()
+    {
+        PDAPanelText.text = "";
+        PDAPanelText.text = "Captains Log, 19081271 \n I found a donut the other day, boy was it tasty, omg killer robots someone HALP pls send the fkn Space marines or some shit \n Oh well im ded now fk";
+    }
+
+    public void PDA2Log()
+    {
+        PDAPanelText.text = "";
+        PDAPanelText.text = "Scientist log 1234 \n Robuts, they are gaining teh smarts \n Need to fix";
+    }
+
+    private void PDAButtonLogHide()
+    {
+        PDA1LogButton.gameObject.SetActive(false);
+        PDA2LogButton.gameObject.SetActive(false);
+    }
+
+    public void CheckPDALog()
+    {
+        if (PDA1Found)
+        {
+            PDA1LogButton.gameObject.SetActive(true);
+        }
+        if (PDA2Found)
+        {
+            PDA2LogButton.gameObject.SetActive(true);
+        }
+
+
+    }
+
 }
