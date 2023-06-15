baf86f3f4f17578c614e6464f15b5ed66015a9cc
Phoques <dylan.munro6@studytafensw.edu.au>
2023-06-15 17:17:04 +1000

    oops

--- Assets/Scripts/Lights.cs
+++ Assets/Scripts/Lights.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 
 public class Lights : MonoBehaviour
 {
+    //This script needs to be attached directly to the object that has the light. 
+    //In this case, the light is its own object ontop of the prefab, so if either have to find component in children, or attach it to the child object which is the light. 
 
     public bool changeLights;
 
-    public Light lightTest;
+    public Light lightTest; // This sets up a variable to be used for the light
 
-    public GameObject lightTestPrefab;
 
     private void Start()
     {
-
+        //This grabs the light component so that now I can change anything in the inspector.
         lightTest = GetComponent<Light>();
     }
 
@@ -23,7 +24,7 @@ public class Lights : MonoBehaviour
     {
         if (changeLights)
         {
-            //lightTest.type = LightType.Spot;
+            //lightTest.type = LightType.Spot; This doesnt appear to be needed. But good to know I suppose in case I want to change the light type?
             lightTest.color = Color.red;
         }
         else
